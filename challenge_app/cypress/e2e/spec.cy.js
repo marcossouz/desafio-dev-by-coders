28e21376209198ffc267d233f936107e1c3b3efd
Marcos Souza <mrcsz.m@gmail.com>
2022-10-21 09:50:01 -0300

    add cypress and docker/docker-compose
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

---
+++ challenge_app/cypress/e2e/spec.cy.js
@@ -0,0 +1,23 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.get('label').contains('LOJA:')
+    
+    cy.get('button').contains('Importar Dados')
+
+    cy.get('select').should('be.visible')
+
+    cy.get('.Transacao_transacoes__Hl6_Q > tbody > :nth-child(1) > :nth-child(1)').contains('Tipo')
+
+    cy.get('button').click()
+
+    cy.url().should('include', '/importacao')
+
+    cy.get(':nth-child(3) > button').contains('Enviar').should('be.visible')
+
+    cy.get('.Importacao_title__v_eiD').contains('Challenge bycoders_')
+
+    cy.get('input').should('be.visible')
+  })
+})
